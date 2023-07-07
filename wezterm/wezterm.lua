33f101e8e3c49c80b29e22cfd37c1ff3adb4eec9
Evergosu <evergosu@gmail.com>
2023-07-07 21:58:54 +0300

    Changed wezterm to use config builder to provide error messages.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,15 @@
 local wezterm = require 'wezterm'
 
-return {
-  font_size = 16.0,
-  color_scheme = 'nord',
-  font = wezterm.font 'FiraCode Nerd Font Mono',
-  hide_tab_bar_if_only_one_tab = true,
-  adjust_window_size_when_changing_font_size = false,
-  window_padding = {
-    top = 20,
-    bottom = 0,
-    right = 0,
-  },
+local c = wezterm.config_builder()
+
+c.font_size = 16.0
+c.color_scheme = 'nord'
+c.font = wezterm.font_with_fallback {
+  'Fira Code Retina',
+  'MesloLGS NF',
 }
+c.hide_tab_bar_if_only_one_tab = true
+c.adjust_window_size_when_changing_font_size = false
+c.force_reverse_video_cursor = true
+
+return c
