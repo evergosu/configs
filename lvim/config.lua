3bcf93feae6eb219fed4d0b12864d9bae5fdbee4
Evergosu <evergosu@gmail.com>
2023-07-08 16:33:21 +0300

    Changed lvim config to match recent plugin updates.

--- lvim/config.lua
+++ lvim/config.lua
@@ -6,44 +6,31 @@ vim.opt.tabstop = 2
 vim.wo.foldlevel = 99
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.diagnostic.config({ virtual_text = false })
 
 lvim.log.level = "warn"
 lvim.colorscheme = "onedarker"
 lvim.transparent_window = true
-lvim.lsp.diagnostics.virtual_text = false
+
+lvim.builtin.treesitter.ensure_installed = {
+	"lua",
+	"rust",
+	"toml",
+}
+require("lvim.lsp.null-ls.formatters").setup({
+	{
+		command = "stylua",
+		filetypes = { "lua" },
+	},
+})
 lvim.format_on_save = {
-  enabled = true,
-  timeout = 1000,
-  pattern = {
-    "*.lua",
-    "*.rs"
-  },
+	enabled = true,
+	timeout = 1000,
+	pattern = {
+		"*.lua",
+		"*.rs",
+	},
 }
-
-lvim.leader = "space"
-lvim.keys.normal_mode["<s-l>"] = ":BufferLineCycleNext<cr>"
-lvim.keys.normal_mode["<s-h>"] = ":BufferLineCyclePrev<cr>"
-lvim.builtin.terminal.open_mapping = "<c-t>"
-lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<cr>", "Projects" }
-lvim.builtin.which_key.mappings["t"] = {
-  name = "Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitioas<cr>", "Definitions" },
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
-}
-lvim.builtin.which_key.mappings["r"] = {
-  name = "Rust tools",
-  a = { "<cmd>RustCodeAction<cr>", "Code" },
-  d = { "<cmd>RustDebuggables<cr>", "Debug" },
-  h = { "<cmd>RustHoverActions<cr>", "Hover" },
-  r = { "<cmd>RustRunnables<cr>", "Run" }
-}
-
-lvim.keys.normal_mode["<c-a>"] = ":DialIncrement<cr>"
-lvim.keys.normal_mode["<c-x>"] = ":DialDecrement<cr>"
 
 lvim.builtin.dap.active = true
 lvim.builtin.alpha.active = true
@@ -55,177 +42,227 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 lvim.builtin.treesitter.auto_install = true
 
+lvim.builtin.terminal.open_mapping = "<c-t>"
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<cr>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+	name = "Trouble",
+	r = { "<cmd>Trouble lsp_references<cr>", "References" },
+	f = { "<cmd>Trouble lsp_definitioas<cr>", "Definitions" },
+	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
+}
+lvim.builtin.which_key.mappings["r"] = {
+	name = "Rust tools",
+	a = { "<cmd>RustCodeAction<cr>", "Code" },
+	d = { "<cmd>RustDebuggables<cr>", "Debug" },
+	h = { "<cmd>RustHoverActions<cr>", "Hover" },
+	r = { "<cmd>RustRunnables<cr>", "Run" },
+}
+
+lvim.leader = "space"
+lvim.keys.normal_mode["<s-l>"] = ":BufferLineCycleNext<cr>"
+lvim.keys.normal_mode["<s-h>"] = ":BufferLineCyclePrev<cr>"
+lvim.keys.normal_mode["<c-a>"] = ":DialIncrement<cr>"
+lvim.keys.normal_mode["<c-x>"] = ":DialDecrement<cr>"
+
 -- Additional Plugins
 lvim.plugins = {
-  {
-    "folke/trouble.nvim",
-    cmd = "Trouble",
-  },
-  {
-    "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("todo-comments").setup()
-    end
-  },
-  {
-    "ggandor/leap.nvim",
-    config = function()
-      require('leap').set_default_keymaps()
-    end
-  },
-  {
-    "karb94/neoscroll.nvim",
-    config = function()
-      require('neoscroll').setup()
-    end
-  },
-  { "lilydjwg/colorizer" },
-  { "tpope/vim-surround" },
-  { "tpope/vim-repeat" },
-  {
-    "monaqa/dial.nvim",
-    config = function()
-      local augend = require("dial.augend")
-      require("dial.config").augends:register_group {
-        default = {
-          augend.semver.alias.semver,
-          augend.integer.alias.decimal,
-          augend.integer.alias.hex,
-          augend.date.alias["%d/%m/%Y"],
-          augend.constant.alias.bool,
-          augend.constant.new {
-            elements = { "let", "const" },
-            word = true,
-            cyclic = true,
-          },
-          augend.constant.new {
-            elements = { "on", "off" },
-            word = true,
-            cyclic = true,
-          },
-          augend.constant.new {
-            elements = { "&&", "||" },
-            word = false,
-            cyclic = true,
-          },
-        },
-      }
-    end
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    requires = { "mfussenegger/nvim-dap" },
-  },
-  { "theHamsta/nvim-dap-virtual-text" },
-  { "nvim-telescope/telescope-dap.nvim" },
-  {
-    "simrat39/rust-tools.nvim",
-    config = function()
-      local extension_path = vim.fn.stdpath "data" .. "/mason/packages/codelldb/extension/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+	{
+		"folke/trouble.nvim",
+		cmd = "Trouble",
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").set_default_keymaps()
+		end,
+	},
+	{
+		"karb94/neoscroll.nvim",
+		config = function()
+			require("neoscroll").setup()
+		end,
+	},
+	{ "lilydjwg/colorizer" },
+	{ "tpope/vim-surround" },
+	{ "tpope/vim-repeat" },
+	{
+		"monaqa/dial.nvim",
+		config = function()
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group({
+				default = {
+					augend.semver.alias.semver,
+					augend.integer.alias.decimal,
+					augend.integer.alias.hex,
+					augend.date.alias["%d/%m/%Y"],
+					augend.constant.alias.bool,
+					augend.constant.new({
+						elements = { "let", "const" },
+						word = true,
+						cyclic = true,
+					}),
+					augend.constant.new({
+						elements = { "on", "off" },
+						word = true,
+						cyclic = true,
+					}),
+					augend.constant.new({
+						elements = { "&&", "||" },
+						word = false,
+						cyclic = true,
+					}),
+				},
+			})
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = "mfussenegger/nvim-dap",
+	},
+	{ "theHamsta/nvim-dap-virtual-text" },
+	{ "nvim-telescope/telescope-dap.nvim" },
+	{
+		"simrat39/rust-tools.nvim",
+		config = function()
+			local extension_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/"
+			local codelldb_path = extension_path .. "adapter/codelldb"
+			local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 
-      local opts = {
-        dap = {
-          adapter = {
-            type = "server",
-            host = "127.0.0.1",
-            port = "${port}",
-            console = "internalConsole",
-            executable = {
-              command = codelldb_path,
-              args = { "--liblldb", liblldb_path, "--port", "${port}" },
-            },
-          }
-        },
-        tools = {
-          executor = require("rust-tools/executors").termopen,
-          reload_workspace_from_cargo_toml = true,
-          inlay_hints = {
-            auto = true,
-            only_current_line = false,
-            show_parameter_hints = true,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "=> ",
-            max_len_align = false,
-            max_len_align_padding = 1,
-            right_align = false,
-            right_align_padding = 7,
-            highlight = "Comment",
-          },
-          runnables = {
-            use_telescope = true
-          },
-          hover_actions = {
-            border = {
-              { "╭", "FloatBorder" },
-              { "─", "FloatBorder" },
-              { "╮", "FloatBorder" },
-              { "│", "FloatBorder" },
-              { "╯", "FloatBorder" },
-              { "─", "FloatBorder" },
-              { "╰", "FloatBorder" },
-              { "│", "FloatBorder" },
-            },
-            auto_focus = true,
-          },
-        },
-        server = {
-          on_init = require("lvim.lsp").common_on_init,
-          on_attach = require("lvim.lsp").common_on_attach,
-          settings = {
-            ["rust-analyzer"] = {
-              imports = {
-                granularity = {
-                  group = "module",
-                },
-                prefix = "self",
-              },
-              cargo = {
-                allFeatures = true,
-                buildScripts = {
-                  enable = true,
-                },
-              },
-              procMacro = {
-                enable = true
-              },
-              checkOnSave = {
-                features = "all",
-                command = "clippy",
-              }
-            }
-          }
-        }
-      }
+			local opts = {
+				dap = {
+					adapter = {
+						type = "server",
+						host = "127.0.0.1",
+						port = "${port}",
+						console = "internalConsole",
+						executable = {
+							command = codelldb_path,
+							args = { "--liblldb", liblldb_path, "--port", "${port}" },
+						},
+					},
+				},
+				tools = {
+					executor = require("rust-tools/executors").termopen,
+					reload_workspace_from_cargo_toml = true,
+					inlay_hints = {
+						auto = true,
+						only_current_line = false,
+						show_parameter_hints = true,
+						parameter_hints_prefix = "",
+						other_hints_prefix = "=> ",
+						max_len_align = false,
+						max_len_align_padding = 1,
+						right_align = false,
+						right_align_padding = 7,
+						highlight = "Comment",
+					},
+					runnables = {
+						use_telescope = true,
+					},
+					hover_actions = {
+						border = "rounded",
+						auto_focus = true,
+					},
+					on_initialized = function()
+						vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+							pattern = { "*.rs" },
+							callback = function()
+								local _, _ = pcall(vim.lsp.codelens.refresh)
+							end,
+						})
+					end,
+				},
+				server = {
+					on_init = require("lvim.lsp").common_on_init,
+					on_attach = function(client, bufnr)
+						require("lvim.lsp").common_on_attach(client, bufnr)
+						local rt = require("rust-tools")
+						vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+					end,
+					capabilities = require("lvim.lsp").common_capabilities(),
+					settings = {
+						["rust-analyzer"] = {
+							lens = {
+								enable = true,
+							},
+							imports = {
+								granularity = {
+									group = "module",
+								},
+								prefix = "self",
+							},
+							cargo = {
+								allFeatures = true,
+								buildScripts = {
+									enable = true,
+								},
+							},
+							procMacro = {
+								enable = true,
+							},
+							checkOnSave = {
+								enable = true,
+								features = "all",
+								command = "clippy",
+							},
+						},
+					},
+				},
+			}
 
-      require("rust-tools").setup(opts)
-    end,
-    ft = { "rust", "rs" },
-  }
+			require("rust-tools").setup(opts)
+		end,
+		ft = { "rust", "rs" },
+	},
+	{
+		"saecki/crates.nvim",
+		version = "v0.3.0",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup({
+				null_ls = {
+					enabled = true,
+					name = "crates.nvim",
+				},
+				popup = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
 }
+
 ---@diagnostic disable-next-line: missing-parameter
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
 
 -- Debug Adapter Protocols
 lvim.builtin.dap.on_config_done = function(dap)
-  local vt = require("nvim-dap-virtual-text");
+	local vt = require("nvim-dap-virtual-text")
 
-  vt.setup { commented = true }
+	vt.setup({ commented = true })
 
-  dap.configurations.rust = {
-    {
-      name = "Debug",
-      type = "rt_lldb",
-      request = "launch",
-      program = "${workspaceFolder}/target/debug/${workspaceFolderBasename}",
-      cwd = "${workspaceFolder}",
-      console = "internalConsole"
-    },
-  }
+	dap.configurations.rust = {
+		{
+			name = "Debug",
+			type = "rt_lldb",
+			request = "launch",
+			program = "${workspaceFolder}/target/debug/${workspaceFolderBasename}",
+			cwd = "${workspaceFolder}",
+			console = "internalConsole",
+		},
+	}
 end
 
 lvim.builtin.telescope.on_config_done = function(telescope)
-  pcall(telescope.load_extension, "dap")
+	pcall(telescope.load_extension, "dap")
 end
