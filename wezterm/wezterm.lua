a5ca41c315912edfd3061228ef06f3f47520ceca
Evergosu <evergosu@gmail.com>
2023-07-12 21:30:06 +0300

    Prettified file with stylua.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,13 +1,13 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local c = wezterm.config_builder()
 
 c.font_size = 16.0
-c.color_scheme = 'nord'
-c.font = wezterm.font_with_fallback {
-  'Fira Code Retina',
-  'MesloLGS NF',
-}
+c.color_scheme = "nord"
+c.font = wezterm.font_with_fallback({
+	"Fira Code Retina",
+	"MesloLGS NF",
+})
 c.hide_tab_bar_if_only_one_tab = true
 c.adjust_window_size_when_changing_font_size = false
 c.force_reverse_video_cursor = true
