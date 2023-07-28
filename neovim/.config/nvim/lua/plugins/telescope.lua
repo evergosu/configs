9d9df0dd9b2a994c5a9ad7a35909697dbbf7d1f5
Evergosu <evergosu@gmail.com>
2023-07-28 21:01:21 +0300

    Added telescope plugin with defaults.

---
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    version = false,
+    keys = {
+      { "<leader>ff", '<cmd>Telescope find_files<cr>', desc = "Find files" },
+      { "<leader>fg", '<cmd>Telescope live_grep<cr>', desc = "Live grep" },
+      { "<leader>fb", '<cmd>Telescope buffers<cr>', desc = "Find buffers" },
+      { "<leader>fh", '<cmd>Telescope help_tags<cr>', desc = "Find help tags" },
+    },
+    opts = {
+      defaults = {
+        mappings = {
+          n = {
+            ["q"] = function(...)
+              return require("telescope.actions").close(...)
+            end,
+          },
+        },
+      },
+    },
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+    cond = function()
+      return vim.fn.executable 'make' == 1
+    end,
+  },
+}
