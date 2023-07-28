968b1b7abd6fdb5ff84b5236a335d61c41a59ccd
Evergosu <evergosu@gmail.com>
2023-07-28 20:12:55 +0300

    Added neo-tree plugin with defaults.

---
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,32 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  cmd = "Neotree",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    { "<leader>fe",
+    function()
+      require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+    end,
+    desc = "[F]ile [E]xplorer", remap = true },
+  },
+  init = function()
+    if vim.fn.argc() == 1 then
+      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("neo-tree")
+      end
+    end
+  end,
+  opts = {
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+      },
+    },
+  },
+}
