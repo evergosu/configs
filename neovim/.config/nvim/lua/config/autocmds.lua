f5b9e8ab1363ed8f9480d3894811a66b37d231dd
Evergosu <evergosu@gmail.com>
2023-07-29 18:37:30 +0300

    Added mason and lsp related plugins.

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,35 @@ local function augroup(name)
   return vim.api.nvim_create_augroup('custom_' .. name, { clear = true })
 end
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = augroup('user_lsp_config'),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>Lwa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>Lwr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>Lwl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>LD', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>Lrn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>Lca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>Lf', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
 -- Makes sure that any opened buffer which is contained in a git repo will be tracked.
 vim.api.nvim_create_autocmd('BufEnter', {
   group = augroup('track_repos'),
