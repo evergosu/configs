9401f1a3c9feec164e78428a0b05a4a145ae5e2d
Evergosu <evergosu@gmail.com>
2023-08-03 14:34:46 +0300

    Improved fzf view.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,11 +3,12 @@ fish_add_path /opt/homebrew/bin
 set -U fish_greeting
 set -Ux LC_ALL C.UTF-8
 set -gx EDITOR nvim
-
 set -gx LS_COLORS (vivid generate nord)
 set -gx GPG_TTY (tty)
 
 set fzf_directory_opts --bind 'ctrl-o:execute($EDITOR {} &> /dev/tty)'
+set fzf_fd_opts --hidden --exclude=.git
+set fzf_preview_dir_cmd ls
 
 fish_vi_key_bindings
 
