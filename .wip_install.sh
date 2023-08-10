f4644eb91a91149968685872904a28ff5a8273bb
Evergosu <evergosu@gmail.com>
2023-08-10 14:57:54 +0300

    Updated instructions to use latest tools.

--- .wip_install.sh
+++ .wip_install.sh
@@ -1,52 +1,23 @@
-#!/bin/zsh
+#!/usr/bin/env sh
 
-echo "Installing vim..."
-bash <(curl -s https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz)
-xattr -c ~/Downloads/nvim-macos.tar.gz
-tar xzvf ~/Downloads/nvim-macos.tar.gz
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+echo "Installing terminal..."
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 echo "Installing CLI tools..."
 xcode-select --install
 
-echo "Installing Brew..."
+echo "Installing package manager..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew analytics off
 
-brew tap homebrew/cask-versions
-brew tap homebrew/cask-fonts
-brew tap FelixKratz/formulae
-brew tap koekeishiya/formulae
-brew tap shaunsingh/SFMono-Nerd-Font-Ligaturized
-
-brew install --cask wezterm-nightly
-brew install --cask font-sf-mono-nerd-font-ligaturized
-brew install vlc
-brew install zoom
-brew install neofetch
-brew install nnn
-brew install mas
-brew install jq
-brew install gh
-brew install stylua
-brew install ripgrep
-brew install dooit --HEAD
-brew install zsh-autosuggestions
-brew install zsh-syntax-highlighting
-brew install zsh-vi-mode
-brew install sketchybar
-brew install sf-symbols
-brew install skhd
-brew install yabai --HEAD
-
+echo "Installing packages..."
+brew bundle
 codesign -fs 'yabai-cert' $(which yabai)
-
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
-source $HOME/.zshrc
-
-echo "Installing Mac App Store apps..."
-mas install 1451685025 # WireguarD
+echo "Installing shell..."
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
 
 echo "Changing MacOS defaults..."
 defaults write com.apple.dock autohide -bool true
@@ -68,11 +39,6 @@ defaults write com.apple.finder ShowPathbar -bool true
 
 # Start Services
 echo "Starting Services (please, grant permissions)..."
-brew services start skhd
-brew services start yabai
 brew services start sketchybar
-
-# neovim
-sudo chown -R $USER ~/.local/share/nvim
-sudo chown -R $USER ~/.cache/nvim
-sudo chown -R $USER ~/.config/
+yabai --start-service
+skhd --start-service
