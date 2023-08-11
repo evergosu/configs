ec0a629bdd8d2c13b80e431ccc580bf3f3561603
Evergosu <evergosu@gmail.com>
2023-08-12 00:01:46 +0300

    Added safe check for strings.

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -13,13 +13,13 @@ return {
       function()
         require('neo-tree.command').execute({ toggle = true, dir = vim.loop.cwd() })
       end,
-      desc = '[F]ile [E]xplorer',
+      desc = '[F]ind in [E]xplorer',
       remap = true,
     },
   },
   init = function()
     if vim.fn.argc() == 1 then
-      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      local stat = vim.loop.fs_stat(tostring(vim.fn.argv(0)))
       if stat and stat.type == 'directory' then
         require('neo-tree')
       end
