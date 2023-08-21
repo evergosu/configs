5ce40e86a618174ebc2331c0be389d048a2d0ea4
Evergosu <evergosu@gmail.com>
2023-08-21 21:28:57 +0300

    Changed lsp float borders.

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -53,6 +53,14 @@ return {
       'folke/neodev.nvim',
     },
     config = function()
+      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = 'double',
+      })
+
+      vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = 'double',
+      })
+
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
