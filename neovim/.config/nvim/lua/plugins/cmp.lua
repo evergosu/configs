468bee9c5a2f2b22dd8a97dbc02558637cab9192
Evergosu <evergosu@gmail.com>
2023-08-28 18:23:07 +0300

    Added completion source for signature help.

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,7 @@
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
+    'hrsh7th/cmp-nvim-lsp-signature-help',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-cmdline',
@@ -60,6 +61,7 @@ return {
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
         { name = 'luasnip' },
       }, {
         { name = 'buffer' },
