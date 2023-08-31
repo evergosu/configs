8f76fcb4d291b2fb6ac099f0ce8faf2d689f08a6
Evergosu <evergosu@gmail.com>
2023-08-31 16:15:29 +0300

    Added rust lsp visual improvements.

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -110,6 +110,15 @@ return {
           local rt = require('rust-tools')
 
           rt.setup({
+            on_initialized = function()
+              vim.cmd([[
+                  augroup RustLSP
+                    autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+                    autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+                    autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+                  augroup END
+                ]])
+            end,
             tools = {
               inlay_hints = {
                 parameter_hints_prefix = '',
