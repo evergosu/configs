23fa6dce33af91ce3964688ad9c391a6df9e2986
Evergosu <evergosu@gmail.com>
2024-08-12 15:40:37 +0300

    Added neovim head to update function.

--- fish/.config/fish/functions/update.fish
+++ fish/.config/fish/functions/update.fish
@@ -1,4 +1,5 @@
 function update --wraps brew --description 'update brew and sketchybar widget'
   brew update; brew upgrade; brew cleanup
+  brew upgrade neovim --fetch-HEAD
   sketchybar --trigger brew_update
 end
