6e3494134534bf47161a1427158cb0af7c6e827e
evergosu <evergosu@gmail.com>
2025-12-24 15:12:26 +0300

    Added pnpm completions for fish.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,18 @@
 fish_add_path /opt/homebrew/bin
 fish_vi_key_bindings
 
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
+set -gx PNPM_HOME /Users/evergosu/Library/pnpm
+fish_add_path $PNPM_HOME
+
 set -U fish_greeting
 
 set -gx LANG en_US.UTF-8
 set -gx LC_ALL en_US.UTF-8
 
 set -gx EDITOR nvim
+set -gx FORCE_COLOR 1
 set -gx LS_COLORS (vivid generate nord)
 
 set -gx GPG_TTY (tty)
